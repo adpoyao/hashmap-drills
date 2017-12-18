307f7ae8c3d60c2cb586cde728ebdcf6dd491405
Eddie Yao <adpoyao@gmail.com>
2017-12-18 08:57:35 -0800

    Adds function stubs and completes get function

--- hashmap.js
+++ hashmap.js
@@ -1,8 +1,46 @@
 'use strict';
 
-class Hashmap {
-  constructor(){
+class HashMap {
+  constructor(initialCapacity = 8){
+    this.length = 0;
+    this._slot = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key){
+    const index = this._findSlot(key);
+    if(this._slot[index] === undefined){
+      throw new Error('Not Found');
+    }
+    return this._slot[index].value;
+  }
+
+  set(key, value){
 
   }
-  
+
+  remove(key){
+
+  }
+
+  _resize(size){
+
+  }
+
+  _findSlot(key){
+    //Hashing Happens Here;
+  }
+
+  static _hashString(string){
+    let hash = 5381;
+    for (let i=0; i<string.length; i++){
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
 }
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
